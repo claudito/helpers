7d038f29cce946abc222c7f9587ea8936ad7ac95
Luis Augusto Claudio Ponce <1107000044@upci.edu.pe>
2021-05-01 10:36:51 -0500

    Create ConfiguracionCorreo.php

---
+++ ConfiguracionCorreo.php
@@ -0,0 +1,24 @@
+<?php
+
+  $config = [
+
+      'driver' => 'SMTP',
+      'host' => 'smtp.gmail.com',
+      'port' => '587',
+      'username' => 'tallerlaravel7notificaciones@gmail.com',
+      'password' => 'taller2021',
+      'encryption'=> 'tls',
+      'from' => [
+
+          'address' => 'tallerlaravel7notificaciones@gmail.com',
+          'name' =>  'tallerlaravel7notificaciones@gmail.com'
+
+      ],
+      'email_reply' => 'tallerlaravel7notificaciones@gmail.com'
+      'email_cc'    => 'tallerlaravel7notificaciones@gmail.com'
+    
+  ];
+
+\Config::set('mail', $config ) );
+Mail::to( $value->correo  )->send( new EnviarCertificadoCorreo( $value ) );
+
